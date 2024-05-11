8f57b9f47f308d4f98ef2d6eaa3281b3fd244bf8
BryanFG01 <bryanfg@hotmail.es>
2024-05-11 12:46:13 -0500

    se obtiene elemntos de Delete y Edit

--- JAVASCRIPT/crud.js
+++ JAVASCRIPT/crud.js
@@ -11,4 +11,25 @@ controller.save = (req, res) =>{
     });
 }; **/
 
+// Obtenemos los elementos de los enlaces
+        const enlaceEdit = document.getElementById("edit");
+        const enlaceDelete = document.getElementById("Delete");
+        
+
+        // Agregamos un evento de clic al enlace "Editar"
+        enlaceEdit.addEventListener("click", function (event) {
+          event.preventDefault(); // Evita que se ejecute la acción predeterminada del enlace
+          console.log("Se ha pulsado el enlace de Editar");
+          // Aquí se agrega el código para abrir un formulario de edición o realizar alguna acción de edición
+        });
+
+
+        // Agregamos un evento de clic al enlace "Eliminar"
+        enlaceDelete.addEventListener("click", function (event) {
+          event.preventDefault(); // Evita que se ejecute la acción predeterminada del enlace
+          console.log("Se ha pulsado el enlace de Eliminar");
+          //  Aquí se agrega el código para eliminar algún elemento o realizar alguna acción
+        });
+
+
 
