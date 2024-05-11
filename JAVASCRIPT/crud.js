f83f77d024efdba5915bb7e4d3cf0c3ed08d5024
BryanFG01 <bryanfg@hotmail.es>
2024-05-10 21:37:36 -0500

    Modifiacion Crudejs / Crudjs

--- JAVASCRIPT/crud.js
+++ JAVASCRIPT/crud.js
@@ -1,2 +1,12 @@
  // poner base de datos en ()
-//const conexion = require(/);
+const conexion = require('/');
+
+controller.save = (req, res) =>{
+    const data = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO nombredelatabla set ?', [data], (err, nomnredetabla) => {
+            res.redirect('/')            
+        });
+    });
+};
